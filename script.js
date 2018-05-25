a410603913dbf67071c5b6eb1897212ee7a36fdb
Sabine Gruber <sab@vollbio.de>
2018-05-25 20:59:52 +0200

    beautify display of items

--- script.js
+++ script.js
@@ -42,12 +42,28 @@ var Event = /** @class */ (function (_super) {
 }(Location));
 var location1 = new Location("St. Charles Church", "Vienna", "1010", "Karlsplatz 1", "img/Karlskirche.jpg");
 var location2 = new Location("Zoo Vienna", "Vienna", "1130", "Maxingstraße 13b", "img/ZooVienna.jpg");
-var restaurant1 = new Restaurant("Lemon Leaf", "Vienna", "1050", "Kettenbrückengasse 19", "img/LemonLeaf.jpg", "+43(1)5812308", "Asian", "www.lemonleaf.at");
-var restaurant2 = new Restaurant("Sixta", "Vienna", "1050", "Schönbrunner Straße 21", "img/Sixta.jpg", "+43 1 58 528 56", "Viennese", "www.sixta-restaurant.at");
+var location3 = new Location("Nationalbibliothek", "Vienna", "1010", "Josefsplatz 1", "img/NB.jpg");
+var restaurant1 = new Restaurant("Lemon Leaf", "Vienna", "1050", "Kettenbrückengasse 19", "img/LemonLeaf.jpg", "+43 15812308", "Asian", "www.lemonleaf.at");
+var restaurant2 = new Restaurant("Sixta", "Vienna", "1050", "Schönbrunner Straße 21", "img/Sixta.jpg", "+43 158528 56", "Viennese", "www.sixta-restaurant.at");
+var restaurant3 = new Restaurant("Pizza Randale", "Vienna", "1050", "Kettenbrückengasse 1", "img/PizzaRandale.jpg", "+43 6508550772", "Pizza", "www.facebook.com/pizzarandale");
 var event1 = new Event("Kris Kristofferson", "Vienna", "1150", "Wiener Stadthalle, Halle F, Roland Rainer Platz 1", "img/Kris_Kristofferson.jpg", "Fr, 15.06.2018", "20:00", "€ 58,50");
 var event2 = new Event("Lenny Krevitz", "Vienna", "1150", "Wiener Stadthalle - Halle D, Roland Rainer Platz 1", "img/Lenny_Kravitz.jpg", "Sat, 09.06.2018", "19:30", "€ 47,80");
 var cityguide_data = [];
-cityguide_data.push(location1, location2, restaurant1, restaurant2, event1, event2);
+cityguide_data.push(location1, location2, location3, restaurant1, restaurant2, restaurant3, event1, event2);
+$(document).ready(function () {
+    for (i = 0; i < cityguide_data.length; i++) {
+        $(".card-img").eq(i).append('<img src="' + cityguide_data[i].Image + '">');
+        $(".card-title").eq(i).append(cityguide_data[i].Name);
+        $(".card-address").eq(i).append(cityguide_data[i].Address);
+        $(".card-zipcode").eq(i).append(cityguide_data[i].ZIPcode);
+        $(".card-city").eq(i).append(cityguide_data[i].City);
+        $(".card-type").eq(i).append(cityguide_data[i].Type);
+        $(".card-date").eq(i).append(cityguide_data[i].EventDate);
+        $(".card-time").eq(i).append(cityguide_data[i].EventTime);
+        $(".card-price").eq(i).append(cityguide_data[i].Price);
+    }
+    ;
+});
 // function render (){
 // 	for (i=0; i<cityguide_data.length; i++){
 // 	blogEntry = document.createElement("div");
@@ -70,17 +86,3 @@ cityguide_data.push(location1, location2, restaurant1, restaurant2, event1, even
 //  	blogEntry.appendChild(zipcode);
 //  }
 // }
-$(document).ready(function () {
-    for (i = 0; i < cityguide_data.length; i++) {
-        $(".card-img").eq(i).append('<img src="' + cityguide_data[i].Image + '">');
-        $(".card-title").eq(i).append(cityguide_data[i].Name);
-        $(".card-address").eq(i).append(cityguide_data[i].Address);
-        $(".card-zipcode").eq(i).append(cityguide_data[i].ZIPcode);
-        $(".card-city").eq(i).append(cityguide_data[i].City);
-        $(".card-type").eq(i).append(cityguide_data[i].Type);
-        $(".card-date").eq(i).append(cityguide_data[i].EventDate);
-        $(".card-time").eq(i).append(cityguide_data[i].EventTime);
-        $(".card-price").eq(i).append(cityguide_data[i].Price);
-    }
-    ;
-});
--- script.ts
+++ script.ts
@@ -23,13 +23,32 @@ class Event extends Location {
 
 let location1 = new Location("St. Charles Church", "Vienna", "1010", "Karlsplatz 1", "img/Karlskirche.jpg")
 let location2 = new Location("Zoo Vienna", "Vienna", "1130", "Maxingstraße 13b", "img/ZooVienna.jpg")
-let restaurant1 = new Restaurant("Lemon Leaf", "Vienna", "1050", "Kettenbrückengasse 19", "img/LemonLeaf.jpg", "+43(1)5812308", "Asian", "www.lemonleaf.at")
-let restaurant2 = new Restaurant("Sixta", "Vienna", "1050", "Schönbrunner Straße 21", "img/Sixta.jpg", "+43 1 58 528 56", "Viennese", "www.sixta-restaurant.at")
+let location3 = new Location("Nationalbibliothek", "Vienna", "1010", "Josefsplatz 1", "img/NB.jpg")
+let restaurant1 = new Restaurant("Lemon Leaf", "Vienna", "1050", "Kettenbrückengasse 19", "img/LemonLeaf.jpg", "+43 15812308", "Asian", "www.lemonleaf.at")
+let restaurant2 = new Restaurant("Sixta", "Vienna", "1050", "Schönbrunner Straße 21", "img/Sixta.jpg", "+43 158528 56", "Viennese", "www.sixta-restaurant.at")
+let restaurant3 = new Restaurant("Pizza Randale", "Vienna", "1050", "Kettenbrückengasse 1", "img/PizzaRandale.jpg", "+43 6508550772", "Pizza", "www.facebook.com/pizzarandale")
 let event1 = new Event("Kris Kristofferson", "Vienna", "1150", "Wiener Stadthalle, Halle F, Roland Rainer Platz 1", "img/Kris_Kristofferson.jpg", "Fr, 15.06.2018", "20:00", "€ 58,50")
 let event2 = new Event("Lenny Krevitz", "Vienna", "1150", "Wiener Stadthalle - Halle D, Roland Rainer Platz 1", "img/Lenny_Kravitz.jpg", "Sat, 09.06.2018", "19:30", "€ 47,80")
 
 let cityguide_data = []
-	cityguide_data.push(location1, location2, restaurant1, restaurant2, event1, event2);
+	cityguide_data.push(location1, location2, location3, restaurant1, restaurant2, restaurant3, event1, event2);
+
+$(document).ready(function() {
+
+for (i = 0; i < cityguide_data.length; i++) {
+        $(".card-img").eq(i).append('<img src="' + cityguide_data[i].Image + '">');
+        $(".card-title").eq(i).append(cityguide_data[i].Name);
+        $(".card-address").eq(i).append(cityguide_data[i].Address);
+        $(".card-zipcode").eq(i).append(cityguide_data[i].ZIPcode);
+        $(".card-city").eq(i).append(cityguide_data[i].City);    
+
+        $(".card-type").eq(i).append(cityguide_data[i].Type);  
+        $(".card-date").eq(i).append(cityguide_data[i].EventDate);  
+        $(".card-time").eq(i).append(cityguide_data[i].EventTime);  
+        $(".card-price").eq(i).append(cityguide_data[i].Price); 
+    };
+
+});
 
 // function render (){
 // 	for (i=0; i<cityguide_data.length; i++){
@@ -54,20 +73,3 @@ let cityguide_data = []
  	
 //  }
 // }
-
-$(document).ready(function() {
-
-for (i = 0; i < cityguide_data.length; i++) {
-        $(".card-img").eq(i).append('<img src="' + cityguide_data[i].Image + '">');
-        $(".card-title").eq(i).append(cityguide_data[i].Name);
-        $(".card-address").eq(i).append(cityguide_data[i].Address);
-        $(".card-zipcode").eq(i).append(cityguide_data[i].ZIPcode);
-        $(".card-city").eq(i).append(cityguide_data[i].City);    
-
-        $(".card-type").eq(i).append(cityguide_data[i].Type);  
-        $(".card-date").eq(i).append(cityguide_data[i].EventDate);  
-        $(".card-time").eq(i).append(cityguide_data[i].EventTime);  
-        $(".card-price").eq(i).append(cityguide_data[i].Price); 
-    };
-
-});
