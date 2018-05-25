c47043adee0bf7d107d554ed25e4f2eee87a26d6
Sabine Gruber <sab@vollbio.de>
2018-05-25 15:29:34 +0200

    push data in array

--- script.js
+++ script.js
@@ -9,36 +9,64 @@ var __extends = (this && this.__extends) || (function () {
     };
 })();
 var Location = /** @class */ (function () {
-    function Location(name, city, ZIPcode, address, image) {
-        this.name = name;
-        this.city = city;
+    function Location(Name, City, ZIPcode, Address, Image) {
+        this.Name = Name;
+        this.City = City;
         this.ZIPcode = ZIPcode;
-        this.address = address;
-        this.image = image;
+        this.Address = Address;
+        this.Image = Image;
     }
     return Location;
 }());
 var Restaurant = /** @class */ (function (_super) {
     __extends(Restaurant, _super);
-    function Restaurant(name, city, ZIPcode, address, image, telephone, type, webaddress) {
-        var _this = this;
+    function Restaurant(Name, City, ZIPcode, Address, Image, Telephone, Type, Webaddress) {
+        var _this = _super.call(this, Name, City, ZIPcode, Address, Image) || this;
+        _this.Telephone = Telephone;
+        _this.Type = Type;
+        _this.Webaddress = Webaddress;
         return _this;
     }
     return Restaurant;
 }(Location));
 var Event = /** @class */ (function (_super) {
     __extends(Event, _super);
-    function Event(name, city, ZIPcode, address, image, EventDate, EventTime, price) {
-        var _this = this;
+    function Event(Name, City, ZIPcode, Address, Image, EventDate, EventTime, Price) {
+        var _this = _super.call(this, Name, City, ZIPcode, Address, Image) || this;
+        _this.EventDate = EventDate;
+        _this.EventTime = EventTime;
+        _this.Price = Price;
         return _this;
     }
     return Event;
 }(Location));
 var location1 = new Location("St. Charles Church", "Vienna", "1010", "Karlsplatz 1", "img/Karlskirche.jpg");
 var location2 = new Location("Zoo Vienna", "Vienna", "1130", "Maxingstraße 13b", "img/ZooVienna.jpg");
-var restaurant1 = new Restaurant("Lemon Leaf Thai Restaurant", "Vienna", "1050", "Kettenbrückengasse 19", "img/LemonLeaf.png");
-var restaurant2 = new Restaurant("Sixta", "Vienna", "1050", "Schönbrunner Straße 21", "img/Sixta.png");
-var event1 = new Event("Kris Kristofferson", "Vienna", "1150", "Wiener Stadthalle, Halle F, Roland Rainer Platz 1", "img/Kris_Kristofferson.jpeg");
-var event2 = new Event("Lenny Krevitz", "Vienna", "1150", "Wiener Stadthalle - Halle D, Roland Rainer Platz 1", "img/Lenny_Kravitz.jpg");
+var restaurant1 = new Restaurant("Lemon Leaf Thai Restaurant", "Vienna", "1050", "Kettenbrückengasse 19", "img/LemonLeaf.png", "+43(1)5812308", "asia", "www.lemonleaf.at");
+var restaurant2 = new Restaurant("Sixta", "Vienna", "1050", "Schönbrunner Straße 21", "img/Sixta.png", "+43 1 58 528 56", "viennese", "www.sixta-restaurant.at");
+var event1 = new Event("Kris Kristofferson", "Vienna", "1150", "Wiener Stadthalle, Halle F, Roland Rainer Platz 1", "img/Kris_Kristofferson.jpeg", "Fr, 15.06.2018", "20:00", "€ 58,50");
+var event2 = new Event("Lenny Krevitz", "Vienna", "1150", "Wiener Stadthalle - Halle D, Roland Rainer Platz 1", "img/Lenny_Kravitz.jpg", "Sat, 09.06.2018", "19:30", "€ 47,80");
 var cityguide_data = [];
 cityguide_data.push(location1, location2, restaurant1, restaurant2, event1, event2);
+function render() {
+    for (i = 0; i < cityguide_data.length; i++) {
+        blogEntry = document.createElement("div");
+        blogEntry.classList.add("col-lg-3");
+        blogEntry.classList.add("col-md-6");
+        blogEntry.classList.add("col-sm-12");
+        var rowEntry = document.getElementById("container");
+        rowEntry.appendChild(blogEntry);
+        var headline = document.createElement("h4");
+        var headline_text = document.createTextNode(cityguide_data[i].Name);
+        headline.appendChild(headline_text);
+        blogEntry.appendChild(headline);
+        var address = document.createElement("p");
+        var address_text = document.createTextNode(cityguide_data[i].Address);
+        address.appendChild(address_text);
+        blogEntry.appendChild(address);
+        var zipcode = document.createElement("p");
+        var zipcode_text = document.createTextNode(cityguide_data[i].ZIPcode);
+        zipcode.appendChild(zipcode_text);
+        blogEntry.appendChild(zipcode);
+    }
+}
--- script.ts
+++ script.ts
@@ -1,25 +1,56 @@
 class Location {
-	constructor(public name, public city, public ZIPcode, public address, public image) {
+	constructor(public Name, public City, public ZIPcode, public Address, public Image) {
 	}
 }
 
 class Restaurant extends Location {
-	constructor(name, city, ZIPcode, address, image, telephone, type, webaddress) {
+	constructor(Name, City, ZIPcode, Address, Image, Telephone, Type, Webaddress) {
+		super(Name, City, ZIPcode, Address, Image);
+		this.Telephone = Telephone;
+		this.Type = Type;
+		this.Webaddress = Webaddress;
 	}
 }
 
 class Event extends Location {
-	constructor(name, city, ZIPcode, address, image, EventDate, EventTime, price) {
+	constructor(Name, City, ZIPcode, Address, Image, EventDate, EventTime, Price) {
+		super(Name, City, ZIPcode, Address, Image);
+		this.EventDate = EventDate;
+		this.EventTime = EventTime;
+		this.Price = Price;
 	}
 }
 
 let location1 = new Location("St. Charles Church", "Vienna", "1010", "Karlsplatz 1", "img/Karlskirche.jpg")
 let location2 = new Location("Zoo Vienna", "Vienna", "1130", "Maxingstraße 13b", "img/ZooVienna.jpg")
-let restaurant1 = new Restaurant("Lemon Leaf Thai Restaurant", "Vienna", "1050", "Kettenbrückengasse 19", "img/LemonLeaf.png")
-let restaurant2 = new Restaurant("Sixta", "Vienna", "1050", "Schönbrunner Straße 21", "img/Sixta.png")
-let event1 = new Event("Kris Kristofferson", "Vienna", "1150", "Wiener Stadthalle, Halle F, Roland Rainer Platz 1", "img/Kris_Kristofferson.jpeg")
-let event2 = new Event("Lenny Krevitz", "Vienna", "1150", "Wiener Stadthalle - Halle D, Roland Rainer Platz 1", "img/Lenny_Kravitz.jpg")
+let restaurant1 = new Restaurant("Lemon Leaf Thai Restaurant", "Vienna", "1050", "Kettenbrückengasse 19", "img/LemonLeaf.png", "+43(1)5812308", "asia", "www.lemonleaf.at")
+let restaurant2 = new Restaurant("Sixta", "Vienna", "1050", "Schönbrunner Straße 21", "img/Sixta.png", "+43 1 58 528 56", "viennese", "www.sixta-restaurant.at")
+let event1 = new Event("Kris Kristofferson", "Vienna", "1150", "Wiener Stadthalle, Halle F, Roland Rainer Platz 1", "img/Kris_Kristofferson.jpeg", "Fr, 15.06.2018", "20:00", "€ 58,50")
+let event2 = new Event("Lenny Krevitz", "Vienna", "1150", "Wiener Stadthalle - Halle D, Roland Rainer Platz 1", "img/Lenny_Kravitz.jpg", "Sat, 09.06.2018", "19:30", "€ 47,80")
 
 let cityguide_data = []
 	cityguide_data.push(location1, location2, restaurant1, restaurant2, event1, event2);
-	
+
+function render (){
+	for (i=0;i<cityguide_data.length;i++){
+	blogEntry = document.createElement("div");
+ 	blogEntry.classList.add("col-lg-3");
+ 	blogEntry.classList.add("col-md-6");
+ 	blogEntry.classList.add("col-sm-12");
+ 	var rowEntry = document.getElementById("container");
+ 	rowEntry.appendChild(blogEntry);
+ 	var headline = document.createElement("h4");
+ 	var headline_text = document.createTextNode(cityguide_data[i].Name);
+ 	headline.appendChild(headline_text);
+ 	blogEntry.appendChild(headline);
+ 	var address = document.createElement("p");
+ 	var address_text = document.createTextNode(cityguide_data[i].Address);
+ 	address.appendChild(address_text);
+ 	blogEntry.appendChild(address);
+ 	var zipcode = document.createElement("p");
+ 	var zipcode_text = document.createTextNode(cityguide_data[i].ZIPcode);
+ 	zipcode.appendChild(zipcode_text);
+ 	blogEntry.appendChild(zipcode);
+ 	
+ }
+}
